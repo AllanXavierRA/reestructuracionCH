6774ec6231143919df06edd79b2eb4a7d5a42261
AXRA <allanraza@hotmail.com>
2023-04-17 17:03:45 -0500

    reestructuracion del proyecto

---
+++ src/dao/model/users.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+import bcrypt from 'bcrypt'
+
+
+const UserSchema =  Schema({
+    name: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        required: [true, 'El email es obligatorio']
+    },
+    password: {
+        type: String
+    }
+})
+
+UserSchema.methods.encryptPassword = async (password) => {
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+
+}
+
+
+UserSchema.methods.matchPassword = async function (password){
+    return await bcrypt.compare(password, this.password);
+}
+
+export const User = model('User', UserSchema)
